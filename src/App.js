710ff84ae936e3dc08e97e7ca81426a6de424463
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-11-02 11:25:48 +0600

    install react router and setup home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
