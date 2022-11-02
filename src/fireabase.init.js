4dd005dbb721ce0b416c67e5d3e33cccbae0b963
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-11-02 12:40:41 +0600

    create navbar and link all route

--- src/fireabase.init.js
+++ src/fireabase.init.js
@@ -8,17 +8,24 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+   /*  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID, */
+  apiKey: "AIzaSyDOOIQNr-0VuUqXSu9ur28IR9T7kJGvokw",
+  authDomain: "bagsq-4ed48.firebaseapp.com",
+  projectId: "bagsq-4ed48",
+  storageBucket: "bagsq-4ed48.appspot.com",
+  messagingSenderId: "581433338558",
+  appId: "1:581433338558:web:b4224cc0fe5baaf7bf4cef",
+  measurementId: "G-HDNFLDPB9T"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 
 export default auth;
