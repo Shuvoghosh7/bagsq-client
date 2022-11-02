f965e865e9adc275e9f07100186faee07facc06b
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-11-02 16:07:28 +0600

    add a hero section where add a image and some text and add react reveal aniation

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Faciliteis from '../Faciliteis/Faciliteis';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <Faciliteis/>
         </div>
     );
 };
