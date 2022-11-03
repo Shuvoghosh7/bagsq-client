799bdc72b2545f8222832771d25db8c8dca2d7b9
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-11-03 18:14:22 +0600

    counter section with react-countup pacages

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import CorporateClient from '../CorporateClient/CorporateClient';
+import CountOurNumbers from '../CountOurNumbers/CountOurNumbers';
 import Faciliteis from '../Faciliteis/Faciliteis';
 import Strength from '../Strength/Strength';
 
@@ -11,6 +12,7 @@ const Home = () => {
             <Faciliteis/>
             <CorporateClient/>
             <Strength/>
+            <CountOurNumbers/>
         </div>
     );
 };
